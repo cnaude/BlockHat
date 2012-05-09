63f5ca03ea8b8f040db145d7b3a9c4d412d60c16
cnaude <cnaude@wilde.ph.cox.net>
2012-05-09 10:48:56 -0700

    Added /unhat

--- src/me/nmc94/BlockHat/BlockHat.java
+++ src/me/nmc94/BlockHat/BlockHat.java
@@ -24,6 +24,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class BlockHat extends JavaPlugin
 {	
 	public static Permission permission = null;
+	public final static String NOPERM = ChatColor.DARK_RED + "You're not allowed to use that command!";
 	// private static Logger log = Logger.getLogger("Minecraft");
 	
 	@Override
@@ -67,125 +68,137 @@ public class BlockHat extends JavaPlugin
 			player = (Player)sender;
 		}
 		
-		if (args.length == 1)
+		if (commandLabel.equalsIgnoreCase("hat") || commandLabel.equalsIgnoreCase("blockhat")) 
 		{
-			if (args[0].equals("help"))
+			if (args.length == 1)
 			{
-				showHelp(command.getName().toLowerCase(), sender);
-			}
-			else if (args[0].startsWith("ver"))
-			{
-				sender.sendMessage(this.getDescription().getFullName());
-			}
-			else if (checkPermission(sender, BlockHatPerm.HAT_ITEMS.node))
-			{
-				ItemStack stack = stackFromString(args[0], 0);
-
-				if ((stack == null) || (stack.getTypeId() > 255) || (stack.getTypeId() < 1))
+				if (args[0].equals("help"))
 				{
-					sender.sendMessage(ChatColor.RED + args[0] + " is not a valid block");
-					return true;
+					showHelp(command.getName().toLowerCase(), sender);
 				}
-				placeOnHead(player, stack);
-			}
-			else
-			{
-				sender.sendMessage(ChatColor.DARK_RED + "Your not allowed to use that command");
-			}
-		}
-		else
-		{
-			Player other;
-			if (args.length == 2)
-			{
-				if (checkPermission(sender, BlockHatPerm.HAT_GIVE_PLAYERS_ITEMS.node))
+				else if (args[0].startsWith("ver"))
 				{
-					ItemStack stack = stackFromString(args[1], 0);
-
+					sender.sendMessage(this.getDescription().getFullName());
+				}
+				else if (checkPermission(sender, BlockHatPerm.HAT_ITEMS.node))
+				{
+					ItemStack stack = stackFromString(args[0], 0);
+	
 					if ((stack == null) || (stack.getTypeId() > 255) || (stack.getTypeId() < 1))
 					{
-						sender.sendMessage(ChatColor.RED + args[1] + " is not a valid block");
+						sender.sendMessage(ChatColor.RED + args[0] + " is not a valid block");
 						return true;
 					}
-
-					List<Player> players = getServer().matchPlayer(args[0]);
-
-					if (players.size() < 1)
-					{
-						sender.sendMessage(ChatColor.RED + "Could not find player");
-					}
-					else if (players.size() > 1)
-					{
-						sender.sendMessage(ChatColor.RED + "More than one player found");
-						String msg = "";
-						for (Iterator<Player> localIterator = players.iterator(); localIterator.hasNext(); )
-						{
-							other = (Player)localIterator.next();
-							msg = msg + " " + other.getName();
-						}
-						sender.sendMessage(msg.trim());
-					}
-					else
-					{
-						Player other1 = (Player)players.get(0);
-						placeOnHead(other1, stack);
-						sender.sendMessage("Putting a block on " + other1.getName() + "'s head.");
-					}
-				}
-				else 
-				{
-					sender.sendMessage(ChatColor.DARK_RED + "Your not allowed to use that command");
-				}
-			}
-			else if ((args.length > 2) && (args[0].equalsIgnoreCase("group")))
-			{
-				if (checkPermission(sender, BlockHatPerm.HAT_GIVE_GROUPS_ITEMS.node))
-				{
-					ItemStack stack = stackFromString(args[2], 0);
-
-					if ((stack == null) || (stack.getTypeId() > 255) || (stack.getTypeId() < 1))
-					{
-						sender.sendMessage(ChatColor.RED + args[2] + " is not a valid block");
-						return true;
-					}
-
-					List<Player> players = new ArrayList<Player>();
-					for (Player player2 : Bukkit.getOnlinePlayers())
-					{
-						if (permission.playerInGroup(player2, args[1]))
-						{
-							players.add(player2);
-						}
-					}
-
-					if (players.size() < 1)
-					{
-						sender.sendMessage(ChatColor.RED + "Could not find any players in " + args[1]);
-					}
-					else
-					{
-						for (Player other2 : players)
-						{
-							placeOnHead(other2, stack);
-						}
-						sender.sendMessage("Putting blocks on players in " + args[1] + " heads.");
-					}
+					placeOnHead(player, stack);
 				}
 				else
 				{
-					sender.sendMessage(ChatColor.DARK_RED + "Your not allowed to use that command");
+					sender.sendMessage(NOPERM);
 				}
-			}
-			else if (checkPermission(sender, BlockHatPerm.HAT.node))
-			{
-				placeOnHead(player, player.getItemInHand());
 			}
 			else
 			{
-				sender.sendMessage(ChatColor.DARK_RED + "Your not allowed to use that command");
+				Player other;
+				if (args.length == 2)
+				{
+					if (checkPermission(sender, BlockHatPerm.HAT_GIVE_PLAYERS_ITEMS.node))
+					{
+						ItemStack stack = stackFromString(args[1], 0);
+	
+						if ((stack == null) || (stack.getTypeId() > 255) || (stack.getTypeId() < 1))
+						{
+							sender.sendMessage(ChatColor.RED + args[1] + " is not a valid block");
+							return true;
+						}
+	
+						List<Player> players = getServer().matchPlayer(args[0]);
+	
+						if (players.size() < 1)
+						{
+							sender.sendMessage(ChatColor.RED + "Could not find player");
+						}
+						else if (players.size() > 1)
+						{
+							sender.sendMessage(ChatColor.RED + "More than one player found");
+							String msg = "";
+							for (Iterator<Player> localIterator = players.iterator(); localIterator.hasNext(); )
+							{
+								other = (Player)localIterator.next();
+								msg = msg + " " + other.getName();
+							}
+							sender.sendMessage(msg.trim());
+						}
+						else
+						{
+							Player other1 = (Player)players.get(0);
+							placeOnHead(other1, stack);
+							sender.sendMessage("Putting a block on " + other1.getName() + "'s head.");
+						}
+					}
+					else 
+					{
+						sender.sendMessage(NOPERM);
+					}
+				}
+				else if ((args.length > 2) && (args[0].equalsIgnoreCase("group")))
+				{
+					if (checkPermission(sender, BlockHatPerm.HAT_GIVE_GROUPS_ITEMS.node))
+					{
+						ItemStack stack = stackFromString(args[2], 0);
+	
+						if ((stack == null) || (stack.getTypeId() > 255) || (stack.getTypeId() < 1))
+						{
+							sender.sendMessage(ChatColor.RED + args[2] + " is not a valid block");
+							return true;
+						}
+	
+						List<Player> players = new ArrayList<Player>();
+						for (Player player2 : Bukkit.getOnlinePlayers())
+						{
+							if (permission.playerInGroup(player2, args[1]))
+							{
+								players.add(player2);
+							}
+						}
+	
+						if (players.size() < 1)
+						{
+							sender.sendMessage(ChatColor.RED + "Could not find any players in " + args[1]);
+						}
+						else
+						{
+							for (Player other2 : players)
+							{
+								placeOnHead(other2, stack);
+							}
+							sender.sendMessage("Putting blocks on players in " + args[1] + " heads.");
+						}
+					}
+					else
+					{
+						sender.sendMessage(NOPERM);
+					}
+				}
+				else if (checkPermission(sender, BlockHatPerm.HAT.node))
+				{
+					placeOnHead(player, player.getItemInHand());
+				}
+				else
+				{
+					sender.sendMessage(NOPERM);
+				}
 			}
-		}
-		return true;
+		} 
+		else if (commandLabel.equalsIgnoreCase("unhat")) 
+		{
+            if (checkPermission(sender, BlockHatPerm.HAT.node)) {
+                ItemStack item = new ItemStack(0);
+                placeOnHead(player, item);
+            } else {
+                sender.sendMessage(NOPERM);
+            }
+        }
+			return true;
 	}
 
 	private void showHelp(String cmd, CommandSender sender)
@@ -217,14 +230,22 @@ public class BlockHat extends JavaPlugin
 	private boolean placeOnHead(Player player, ItemStack item)
 	{
 		PlayerInventory inv = player.getInventory();
-		if (item.getType() == Material.AIR)
-		{
-			player.sendMessage(ChatColor.RED + "Why would you want air on your head?");
-			return false;
-		}
+		ArrayList<Integer> validTypes = new ArrayList<Integer>();
+        validTypes.add(298); //Leather helmet
+        validTypes.add(302); //Chainmail helmet
+        validTypes.add(306); //Iron helmet
+        validTypes.add(310); //Diamond helmet
+        validTypes.add(314); //Gold helmet
+        
+        // Placing AIR on the head is a safe way to remove the hat.
+		//if (item.getType() == Material.AIR)
+		//{
+		//	player.sendMessage(ChatColor.RED + "Why would you want air on your head?");
+		//	return false;
+		//}
 
 		int id = item.getTypeId();
-		if ((id < 0) || (id > 255))
+		if ((id >= 0 && id <= 255) || validTypes.contains(id))
 		{
 			player.sendMessage(ChatColor.RED + "You can't put that item on your head silly!");
 			return false;
@@ -243,8 +264,8 @@ public class BlockHat extends JavaPlugin
 			item.setAmount(item.getAmount() - 1);
 		}
 		else
-		{
-			inv.remove(item);
+		{			
+			inv.removeItem(item);
 		}
 
 		if (helmet != null && helmet.getAmount() > 0)
@@ -260,7 +281,14 @@ public class BlockHat extends JavaPlugin
 				}
 			}
 		}
-		player.sendMessage("Enjoy your new hat!");
+		if (item.getType() == Material.AIR) 
+		{
+			player.sendMessage("No longer wearing a new hat!");
+		} 
+		else
+		{
+			player.sendMessage("Enjoy your new hat!");
+		}
 		return true;
 	}
 
